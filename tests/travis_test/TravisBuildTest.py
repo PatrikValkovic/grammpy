3f42b18de3c1f6c5b15e003d7e7c68b0d3c04fb4
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-06-23 14:13:52 +0200

    Remove option to run test file directly

--- tests/travis_test/TravisBuildTest.py
+++ tests/travis_test/TravisBuildTest.py
@@ -4,11 +4,3 @@ import unittest as ut
 class TravisBuildTest(ut.TestCase):
     def test_success(self):
         self.assertEqual(1, 1, "1 is not equal to 1?!")
-
-
-def main():
-    ut.main()
-
-
-if __name__ == '__main__':
-    main()
