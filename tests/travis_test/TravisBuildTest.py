12141178659f5ad7241de96d3675d4b671ffc64a
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-06-23 13:42:33 +0200

    Change name of methods in test case so they are execute

--- tests/travis_test/TravisBuildTest.py
+++ tests/travis_test/TravisBuildTest.py
@@ -2,10 +2,10 @@ import unittest as ut
 
 
 class TravisBuildTest(ut.TestCase):
-    def success_test(self):
+    def test_success(self):
         self.assertEqual(1, 1, "1 is not equal to 1?!")
 
-    def failed_test(self):
+    def test_failing(self):
         self.assertEqual(1, 2, "1 is not equal to 2")
 
 
