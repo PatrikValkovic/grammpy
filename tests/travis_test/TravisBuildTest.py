f2de9920691a3f3f33f60da7edfcfabadaf37044
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-06-23 13:35:34 +0200

    Add simple unit test, that everything works on travis

---
+++ tests/travis_test/TravisBuildTest.py
@@ -0,0 +1,14 @@
+import unittest as ut
+
+
+class TravisBuildTest(ut.TestCase):
+    def test(self):
+        self.assertEqual(1, 1, "1 is not equal to 1?!")
+
+
+def main():
+    ut.main()
+
+
+if __name__ == '__main__':
+    main()
