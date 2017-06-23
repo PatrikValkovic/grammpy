b10f4690bde4de357600c2ceb7062bfed51d725f
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-06-23 16:45:43 +0200

    Add files for Grammar, Rule and factory

---
+++ grammpy/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+:Author Patrik Valkovic
+:Created 23.06.2017 16:38
+:Licence GNUv3
+Part of grammpy
+
+"""
+
+from .Grammar import Grammar
+from .Rule import Rule
+from . import factory
