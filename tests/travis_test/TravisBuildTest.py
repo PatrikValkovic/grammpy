d0f425d215f0d1c4f57a3517ad3e4c15f2b35e86
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-06-23 13:50:56 +0200

    Add failing test to dev branch to test, if master badge stay green

--- tests/travis_test/TravisBuildTest.py
+++ tests/travis_test/TravisBuildTest.py
@@ -5,6 +5,9 @@ class TravisBuildTest(ut.TestCase):
     def test_success(self):
         self.assertEqual(1, 1, "1 is not equal to 1?!")
 
+    def test_failing(self):
+        self.assertEqual(1, 2)
+
 
 def main():
     ut.main()
