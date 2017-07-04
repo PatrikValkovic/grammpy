417b3caf2a64a68e0da56b3caa9213c2c64087c3
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-07-04 19:45:20 +0200

    Add class represents terminal symbol

---
+++ grammpy/Terminal.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""
+:Author Patrik Valkovic
+:Created 23.06.2017 16:39
+:Licence GNUv3
+Part of grammpy
+
+"""
+
+
+class Terminal:
+    def __init__(self, symbol, grammar):
+        self.__symbol = symbol
+        self.__grammar = grammar
+
+    def __hash__(self):
+        return hash((self.__symbol, id(self.__grammar)))
+    
+    def __eq__(self, other):
+        return isinstance(other, Terminal) and hash(self) == hash(other)
