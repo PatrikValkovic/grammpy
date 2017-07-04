39327cb18d3551618e84aaa2f70f5e3baaf734de
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-07-04 20:19:40 +0200

    Add test for correct handling of grammar in terminal

---
+++ tests/terminal_tests/CorrectGrammarHandlingTest.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+"""
+:Author Patrik Valkovic
+:Created 23.06.2017 16:39
+:Licence GNUv3
+Part of grammpy
+
+"""
+from unittest import TestCase
+from grammpy import Terminal
+
+
+class TempClass:
+    pass
+
+
+class CorrectGrammarHandlingTest(TestCase):
+    def test_sameNumber(self):
+        ter1 = Terminal(0, 1)
+        ter2 = Terminal(0, 1)
+        self.assertEqual(ter1, ter2)
+
+    def test_sameString(self):
+        ter1 = Terminal(0, 'a')
+        ter2 = Terminal(0, 'a')
+        self.assertEqual(ter1, ter2)
+
+    def test_sameClass(self):
+        ter1 = Terminal(0, TempClass)
+        ter2 = Terminal(0, TempClass)
+        self.assertEqual(ter1, ter2)
+
+    def test_sameInstance(self):
+        inst = TempClass()
+        ter1 = Terminal(0, inst)
+        ter2 = Terminal(0, inst)
+        self.assertEqual(ter1, ter2)
