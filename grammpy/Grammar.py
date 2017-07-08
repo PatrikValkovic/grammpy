baea596f542020ad420f11037fdaca30477f9d14
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-07-08 12:05:07 +0200

    Add Grammar API for terminals and nonterminals

--- grammpy/Grammar.py
+++ grammpy/Grammar.py
@@ -9,4 +9,36 @@ Part of grammpy
 
 
 class Grammar:
+
+    def __init__(self):
+        self.__terminals = set()
+        self.__nonterminals = set()
+
+
+    #Term part
+    def add_term(self, term):
+        raise NotImplementedError()
+
+    def remove_term(self, term = None):
+        raise NotImplementedError()
+
+    def have_term(self, term):
+        raise NotImplementedError()
+
+    def get_term(self, term):
+        raise NotImplementedError()
+
+    #Non term part
+    def add_nonterm(self, nonterm):
+        raise NotImplementedError()
+
+    def remove_nonterm(self, nonterm=None):
+        raise NotImplementedError()
+
+    def have_nonterm(self, nonterm):
+        raise NotImplementedError()
+
+    def get_nonterm(self, nonterm):
+        raise NotImplementedError()
+
     pass
