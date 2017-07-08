cea38c682e5c6d4b5af7e4716433af3901ce694e
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-07-08 13:55:22 +0200

    Add tests - adding terminals into grammar

---
+++ tests/grammar_adding_test/TerminalAddingTest.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+"""
+:Author Patrik Valkovic
+:Created 23.06.2017 16:39
+:Licence GNUv3
+Part of grammpy
+
+"""
+from unittest import TestCase
+from grammpy import Grammar
+from grammpy.Terminal import Terminal
+
+
+class TerminalAddingTest(TestCase):
+    def test_correctAddOne(self):
+        gr = Grammar()
+        self.assertIsNone(gr.get_term(0))
+        self.assertIsNone(gr.term(0))
+        gr.add_term(0)
+        self.assertIsNotNone(gr.get_term(0))
+        self.assertIsNotNone(gr.term(0))
+        self.assertTrue(isinstance(gr.term(0), Terminal))
+        self.assertEqual(gr.term(0).symbol(), 0)
+
+    def test_correctAddTwo(self):
+        gr = Grammar()
+        self.assertIsNone(gr.get_term(0))
+        self.assertIsNone(gr.term(0))
+        self.assertIsNone(gr.get_term('asdf'))
+        self.assertIsNone(gr.term('asdf'))
+        gr.add_term(0)
+        self.assertIsNotNone(gr.get_term(0))
+        self.assertIsNotNone(gr.term(0))
+        self.assertTrue(isinstance(gr.term(0), Terminal))
+        self.assertEqual(gr.term(0).symbol(), 0)
+        self.assertIsNone(gr.get_term('asdf'))
+        self.assertIsNone(gr.term('asdf'))
+        gr.add_term('asdf')
+        self.assertIsNotNone(gr.get_term(0))
+        self.assertIsNotNone(gr.term(0))
+        self.assertTrue(isinstance(gr.term(0), Terminal))
+        self.assertEqual(gr.term(0).symbol(), 0)
+        self.assertIsNotNone(gr.get_term('asdf'))
+        self.assertIsNotNone(gr.term('asdf'))
+        self.assertTrue(isinstance(gr.term('asdf'), Terminal))
+        self.assertEqual(gr.term('asdf').symbol(), 'asdf')
