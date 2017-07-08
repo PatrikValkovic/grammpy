47c69e3d4f96d4bb209ab6f739f7dd60111b11c4
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-07-08 16:51:16 +0200

    Add tests for remove terms

---
+++ tests/grammar_term-nonterm_test/TerminalRemoveTest.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python
+"""
+:Author Patrik Valkovic
+:Created 23.06.2017 16:39
+:Licence GNUv3
+Part of grammpy
+
+"""
+from unittest import TestCase
+from grammpy import Grammar
+from grammpy.Terminal import Terminal
+
+
+class TempClass:
+    pass
+
+
+class TerminalRemoveTest(TestCase):
+    def test_removeOne(self):
+        gr = Grammar()
+        gr.add_term([0, 'asdf', TempClass])
+        self.assertEqual(gr.terms_count(), 3)
+        gr.remove_term(0)
+        self.assertEqual(gr.terms_count(), 2)
+        self.assertTrue(gr.have_term('asdf'))
+        self.assertTrue(gr.have_term(TempClass))
+        self.assertFalse(gr.have_term(0))
+
+    def test_removeClass(self):
+        gr = Grammar()
+        gr.add_term([0, 'asdf', TempClass])
+        self.assertEqual(gr.terms_count(), 3)
+        gr.remove_term(TempClass)
+        self.assertEqual(gr.terms_count(), 2)
+        self.assertTrue(gr.have_term('asdf'))
+        self.assertTrue(gr.have_term(0))
+        self.assertFalse(gr.have_term(TempClass))
+
+    def test_removeTwo(self):
+        gr = Grammar()
+        gr.add_term([0, 'asdf', TempClass])
+        self.assertEqual(gr.terms_count(), 3)
+        gr.remove_term(0)
+        gr.remove_term('asdf')
+        self.assertEqual(gr.terms_count(), 1)
+        self.assertTrue(gr.have_term(TempClass))
+        self.assertFalse(gr.have_term('asdf'))
+        self.assertFalse(gr.have_term(0))
+
+    def test_removeTwoInArray(self):
+        gr = Grammar()
+        gr.add_term([0, 'asdf', TempClass])
+        self.assertEqual(gr.terms_count(), 3)
+        gr.remove_term([0, 'asdf'])
+        self.assertEqual(gr.terms_count(), 1)
+        self.assertTrue(gr.have_term(TempClass))
+        self.assertFalse(gr.have_term('asdf'))
+        self.assertFalse(gr.have_term(0))
+
+    def test_removeTwoInTuple(self):
+        gr = Grammar()
+        gr.add_term([0, 'asdf', TempClass])
+        self.assertEqual(gr.terms_count(), 3)
+        gr.remove_term((0, 'asdf'))
+        self.assertEqual(gr.terms_count(), 1)
+        self.assertTrue(gr.have_term(TempClass))
+        self.assertFalse(gr.have_term('asdf'))
+        self.assertFalse(gr.have_term(0))
+
+    def test_removeAllWithoutParam(self):
+        gr = Grammar()
+        gr.add_term([0, 'asdf', TempClass])
+        self.assertEqual(gr.terms_count(), 3)
+        gr.remove_term()
+        self.assertEqual(gr.terms_count(), 0)
+        self.assertFalse(gr.have_term(TempClass))
+        self.assertFalse(gr.have_term('asdf'))
+        self.assertFalse(gr.have_term(0))
+
+    def test_removeEmptyGrammar(self):
+        gr = Grammar()
+        self.assertEqual(gr.terms_count(), 0)
+        gr.remove_term()
+        self.assertEqual(gr.terms_count(), 0)
