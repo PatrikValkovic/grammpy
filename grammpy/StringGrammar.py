f72cf28dd701aace68c329467f424931b97430bb
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-07-28 23:51:45 +0200

    Create class StringGrammar

---
+++ grammpy/StringGrammar.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+"""
+:Author Patrik Valkovic
+:Created 23.06.2017 16:39
+:Licence GNUv3
+Part of grammpy
+
+"""
+
+from .RawGrammar import RawGrammar
+
+class StringGrammar(RawGrammar):
+    pass
