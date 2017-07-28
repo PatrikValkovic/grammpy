737bf244f36b73a54b5b4f89f0c7e604d3f34b72
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-07-28 10:58:46 +0200

    Add tests of get nonterms

--- tests/grammar_term-nonterm_test/NonterminalGetTest.py
+++ tests/grammar_term-nonterm_test/NonterminalGetTest.py
@@ -25,7 +25,49 @@ class Third(Nonterminal):
 
 
 class TerminalGetTest(TestCase):
-    pass
+    def test_getNontermEmpty(self):
+        gr = Grammar()
+        self.assertIsNone(gr.get_nonterm(TempClass))
+        self.assertIsNone(gr.get_nonterm(Second))
+        self.assertIsNone(gr.get_nonterm(Third))
+
+    def test_getNontermClass(self):
+        gr = Grammar()
+        gr.add_nonterm(TempClass)
+        self.assertEqual(gr.get_nonterm(TempClass), TempClass)
+
+    def test_getNontermArray(self):
+        gr = Grammar()
+        gr.add_nonterm([TempClass, Second, Third])
+        g = gr.get_term([Second, TempClass])
+        for i in g:
+            self.assertTrue(i in [TempClass, Second, Third])
+        self.assertEqual(g[0], Second)
+        self.assertEqual(g[1], TempClass)
+
+    def test_dontGetNontermArray(self):
+        gr = Grammar()
+        gr.add_term([TempClass, Second])
+        g = gr.get_term([TempClass, Third])
+        self.assertEqual(g[0], TempClass)
+        self.assertIsNone(g[1])
+
+    def test_getNontermTuple(self):
+        gr = Grammar()
+        gr.add_term([TempClass, Second, Third])
+        g = gr.get_term((Third, TempClass))
+        for i in g:
+            self.assertTrue(i in [TempClass, Second, Third])
+        self.assertEqual(g[0], Third)
+        self.assertEqual(g[1], TempClass)
+
+    def test_dontGetNontermTuple(self):
+        gr = Grammar()
+        gr.add_term([TempClass, Second])
+        g = gr.get_term((TempClass, Third))
+        self.assertEqual(g[0], TempClass)
+        self.assertIsNone(g[1])
+
 
 if __name__ == '__main__':
     main()
