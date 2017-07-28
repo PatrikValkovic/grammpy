85d2c012bfaeeb04fa8dd31cd05a04a8dc43c14e
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-07-28 19:33:27 +0200

    Add tests that have and get of nonterms raise exceptions

--- tests/grammar_term-nonterm_test/NonterminalsInvalidTest.py
+++ tests/grammar_term-nonterm_test/NonterminalsInvalidTest.py
@@ -7,13 +7,57 @@ Part of grammpy
 
 """
 from unittest import TestCase, main
-from grammpy.RawGrammar import RawGrammar
+from grammpy.RawGrammar import RawGrammar as Grammar
+from grammpy import Nonterminal
+from grammpy.exceptions import NotNonterminalException
 
 
-class NonterminalsInvalidTest(TestCase):
+class TempClass(Nonterminal):
     pass
 
 
+class NonterminalsInvalidTest(TestCase):
+    def test_invalidAddNumber(self):
+        gr = Grammar()
+        with self.assertRaises(NotNonterminalException):
+            gr.add_nonterm(0)
+
+    def test_invalidAddString(self):
+        gr = Grammar()
+        with self.assertRaises(NotNonterminalException):
+            gr.add_nonterm("string")
+
+    def test_invalidAddAfterCorrectAdd(self):
+        gr = Grammar()
+        gr.add_nonterm(TempClass)
+        with self.assertRaises(NotNonterminalException):
+            gr.add_nonterm("asdf")
+
+    def test_invalidAddInArray(self):
+        gr = Grammar()
+        with self.assertRaises(NotNonterminalException):
+            gr.add_nonterm([TempClass, "asdf"])
+
+    def test_invalidHaveNumber(self):
+        gr = Grammar()
+        with self.assertRaises(NotNonterminalException):
+            gr.have_nonterm(0)
+
+    def test_invalidHaveString(self):
+        gr = Grammar()
+        with self.assertRaises(NotNonterminalException):
+            gr.have_nonterm("string")
+
+    def test_invalidHaveAfterCorrectAdd(self):
+        gr = Grammar()
+        gr.add_nonterm(TempClass)
+        with self.assertRaises(NotNonterminalException):
+            gr.have_nonterm("asdf")
+
+    def test_invalidHaveInArray(self):
+        gr = Grammar()
+        with self.assertRaises(NotNonterminalException):
+            gr.have_nonterm([TempClass, "asdf"])
 
 
 if __name__ == '__main__':
