e43421683538c8b60163b9f12cab9869054ddeb6
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-07-29 00:09:36 +0200

    Add base implementation of StringGrammar

--- grammpy/StringGrammar.py
+++ grammpy/StringGrammar.py
@@ -9,5 +9,25 @@ Part of grammpy
 
 from .RawGrammar import RawGrammar as Grammar
 
+
 class StringGrammar(Grammar):
-    pass
+    @staticmethod
+    def __to_string_arr(t):
+        if isinstance(t,str):
+            return [t]
+        return t
+
+    def remove_term(self, term=None):
+        super().remove_term(StringGrammar.__to_string_arr(term))
+
+    def add_term(self, term):
+        super().add_term(StringGrammar.__to_string_arr(term))
+
+    def term(self, term=None):
+        return super().term(StringGrammar.__to_string_arr(term))
+
+    def get_term(self, term=None):
+        return super().get_term(StringGrammar.__to_string_arr(term))
+
+    def have_term(self, term):
+        return super().have_term(StringGrammar.__to_string_arr(term))
