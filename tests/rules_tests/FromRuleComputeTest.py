41bda27ef85850c9c51c52ab61edc408070a0121
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-01 14:00:29 +0200

    Add tests to compute rules from rule

--- tests/rules_tests/FromRuleComputeTest.py
+++ tests/rules_tests/FromRuleComputeTest.py
@@ -11,8 +11,72 @@ from unittest import main, TestCase
 from grammpy import Rule
 
 
+class Single(Rule):
+    rule = ([0], [1])
+
+
+class TwoRight(Rule):
+    rule = ([0], [1, 2])
+
+
+class ThreeLeft(Rule):
+    rule = ([0, 1, 'a'], [2])
+
+
+class Multiple(Rule):
+    rule = ([0, 1, 2], [3, 4])
+
+
 class FromRuleComputeTest(TestCase):
-    pass
+    def test_rules_single(self):
+        r = Single.rules
+        self.assertIsInstance(r, list)
+        self.assertEqual(len(r), 1)
+        self.assertEqual(r[0], Single.rule)
+        self.assertIsInstance(r[0], tuple)
+        self.assertEqual(r[0][0], [0])
+        self.assertEqual(r[0][1], [1])
+        self.assertEqual(r[0][0][0], 0)
+        self.assertEqual(r[0][1][0], 1)
+
+    def test_rules_twoRight(self):
+        r = TwoRight.rules
+        self.assertIsInstance(r, list)
+        self.assertEqual(len(r), 1)
+        self.assertEqual(r[0], TwoRight.rule)
+        self.assertIsInstance(r[0], tuple)
+        self.assertEqual(r[0][0], [0])
+        self.assertEqual(r[0][1], [1, 2])
+        self.assertEqual(r[0][0][0], 0)
+        self.assertEqual(r[0][1][0], 1)
+        self.assertEqual(r[0][1][1], 2)
+
+    def test_rules_threeLeft(self):
+        r = ThreeLeft.rules
+        self.assertIsInstance(r, list)
+        self.assertEqual(len(r), 1)
+        self.assertEqual(r[0], ThreeLeft.rule)
+        self.assertIsInstance(r[0], tuple)
+        self.assertEqual(r[0][0], [0, 1, 'a'])
+        self.assertEqual(r[0][1], [2])
+        self.assertEqual(r[0][0][0], 0)
+        self.assertEqual(r[0][0][1], 1)
+        self.assertEqual(r[0][0][2], 'a')
+        self.assertEqual(r[0][1][0], 2)
+
+    def test_rules_multiple(self):
+        r = Multiple.rules
+        self.assertIsInstance(r, list)
+        self.assertEqual(len(r), 1)
+        self.assertEqual(r[0], Multiple.rule)
+        self.assertIsInstance(r[0], tuple)
+        self.assertEqual(r[0][0], [0, 1, 2])
+        self.assertEqual(r[0][1], [3, 4])
+        self.assertEqual(r[0][0][0], 0)
+        self.assertEqual(r[0][0][1], 1)
+        self.assertEqual(r[0][0][2], 2)
+        self.assertEqual(r[0][1][0], 3)
+        self.assertEqual(r[0][1][1], 4)
 
 
 if __name__ == '__main__':
