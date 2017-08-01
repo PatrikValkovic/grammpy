1f3625b98de4edd8a0d0bcce882c5f79aeaf23b2
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-01 16:03:16 +0200

    Add file for tests, when no rule is specified

---
+++ tests/rules_tests/NoRuleSpecifiedTest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""
+:Author Patrik Valkovic
+:Created 23.06.2017 16:39
+:Licence GNUv3
+Part of grammpy
+
+"""
+
+from unittest import main, TestCase
+from grammpy import Rule
+
+
+class NoRuleSpecifiedTest(TestCase):
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
