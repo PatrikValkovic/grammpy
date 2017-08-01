678555e87d053e70ee5bca6499b16a06618dd1a7
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-01 16:25:08 +0200

    Import IsMethodsRuleExtension by default

--- grammpy/__init__.py
+++ grammpy/__init__.py
@@ -9,5 +9,5 @@ Part of grammpy
 
 from .Grammars.PrettyApiGrammar import PrettyApiGrammar as Grammar
 from .Nonterminal import Nonterminal
-from .Rule import Rule
+from .IsMethodsRuleExtension import IsMethodsRuleExtension as Rule
 from .Constants import EPSILON, EPS
