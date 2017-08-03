dcd22f08d546190bfb4b35e0181393c6f342ae7a
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-03 12:12:43 +0200

    Add tests of adding only one rule

--- tests/rules_tests/grammarManipulation_tests/RuleAddingTest.py
+++ tests/rules_tests/grammarManipulation_tests/RuleAddingTest.py
@@ -8,7 +8,7 @@ Part of grammpy
 """
 
 from unittest import main, TestCase
-from grammpy import Rule, Grammar, Nonterminal as _N
+from grammpy import Rule as _R, Grammar, Nonterminal as _N
 
 
 class NFirst(_N):
@@ -28,14 +28,48 @@ class NFourth(_N):
 
 
 class RuleAddingTest(TestCase):
-    def __init__(self):
-        super().__init__()
-        self.g = None
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.g = Grammar()
 
     def setUp(self):
         g = Grammar()
         g.add_term([0, 1, 2, 'a', 'b', 'c'])
         g.add_nonterm([NFirst, NSecond, NThird, NFourth])
+        self.g = g
+
+    def test_addOne(self):
+        class Tmp(_R):
+            rule = ([NFirst], ['a', 0])
+        self.assertFalse(self.g.have_rule(Tmp))
+        self.assertIsNone(self.g.get_rule(Tmp))
+        self.assertIsNone(self.g.rule(Tmp))
+        self.g.add_rule(Tmp)
+        self.assertTrue(self.g.have_rule(Tmp))
+        self.assertEqual(self.g.get_rule(Tmp), Tmp)
+        self.assertEqual(self.g.rule(Tmp), Tmp)
+
+    def test_addOneInArray(self):
+        class Tmp(_R):
+            rule = ([NFirst], ['a', 0])
+        self.assertFalse(self.g.have_rule(Tmp))
+        self.assertIsNone(self.g.get_rule(Tmp))
+        self.assertIsNone(self.g.rule(Tmp))
+        self.g.add_rule([Tmp])
+        self.assertTrue(self.g.have_rule(Tmp))
+        self.assertEqual(self.g.get_rule(Tmp), Tmp)
+        self.assertEqual(self.g.rule(Tmp), Tmp)
+
+    def test_addOneInTuple(self):
+        class Tmp(_R):
+            rule = ([NFirst], ['a', 0])
+        self.assertFalse(self.g.have_rule(Tmp))
+        self.assertIsNone(self.g.get_rule(Tmp))
+        self.assertIsNone(self.g.rule(Tmp))
+        self.g.add_rule((Tmp,))
+        self.assertTrue(self.g.have_rule(Tmp))
+        self.assertEqual(self.g.get_rule(Tmp), Tmp)
+        self.assertEqual(self.g.rule(Tmp), Tmp)
 
 
 if __name__ == '__main__':
