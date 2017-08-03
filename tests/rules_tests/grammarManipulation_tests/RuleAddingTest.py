4a2112d6ae80fcb7d428582c07f687b4802a854a
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-03 12:07:54 +0200

    Add creation of grammar before each rule's add tests

--- tests/rules_tests/grammarManipulation_tests/RuleAddingTest.py
+++ tests/rules_tests/grammarManipulation_tests/RuleAddingTest.py
@@ -8,11 +8,34 @@ Part of grammpy
 """
 
 from unittest import main, TestCase
-from grammpy import Rule, Grammar, Nonterminal
+from grammpy import Rule, Grammar, Nonterminal as _N
+
+
+class NFirst(_N):
+    pass
+
+
+class NSecond(_N):
+    pass
+
+
+class NThird(_N):
+    pass
+
+
+class NFourth(_N):
+    pass
 
 
 class RuleAddingTest(TestCase):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.g = None
+
+    def setUp(self):
+        g = Grammar()
+        g.add_term([0, 1, 2, 'a', 'b', 'c'])
+        g.add_nonterm([NFirst, NSecond, NThird, NFourth])
 
 
 if __name__ == '__main__':
