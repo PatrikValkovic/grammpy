0728d8f42e00ce7cf0d310fdc32fe7cbb5f5bf9a
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-03 12:21:13 +0200

    Add tests of adding more rules

--- tests/rules_tests/grammarManipulation_tests/RuleAddingTest.py
+++ tests/rules_tests/grammarManipulation_tests/RuleAddingTest.py
@@ -41,10 +41,12 @@ class RuleAddingTest(TestCase):
     def test_addOne(self):
         class Tmp(_R):
             rule = ([NFirst], ['a', 0])
+        self.assertEqual(self.g.rules_count(), 0)
         self.assertFalse(self.g.have_rule(Tmp))
         self.assertIsNone(self.g.get_rule(Tmp))
         self.assertIsNone(self.g.rule(Tmp))
         self.g.add_rule(Tmp)
+        self.assertEqual(self.g.rules_count(), 1)
         self.assertTrue(self.g.have_rule(Tmp))
         self.assertEqual(self.g.get_rule(Tmp), Tmp)
         self.assertEqual(self.g.rule(Tmp), Tmp)
@@ -52,10 +54,12 @@ class RuleAddingTest(TestCase):
     def test_addOneInArray(self):
         class Tmp(_R):
             rule = ([NFirst], ['a', 0])
+        self.assertEqual(self.g.rules_count(), 0)
         self.assertFalse(self.g.have_rule(Tmp))
         self.assertIsNone(self.g.get_rule(Tmp))
         self.assertIsNone(self.g.rule(Tmp))
         self.g.add_rule([Tmp])
+        self.assertEqual(self.g.rules_count(), 1)
         self.assertTrue(self.g.have_rule(Tmp))
         self.assertEqual(self.g.get_rule(Tmp), Tmp)
         self.assertEqual(self.g.rule(Tmp), Tmp)
@@ -63,13 +67,106 @@ class RuleAddingTest(TestCase):
     def test_addOneInTuple(self):
         class Tmp(_R):
             rule = ([NFirst], ['a', 0])
+        self.assertEqual(self.g.rules_count(), 0)
         self.assertFalse(self.g.have_rule(Tmp))
         self.assertIsNone(self.g.get_rule(Tmp))
         self.assertIsNone(self.g.rule(Tmp))
         self.g.add_rule((Tmp,))
+        self.assertEqual(self.g.rules_count(), 1)
         self.assertTrue(self.g.have_rule(Tmp))
         self.assertEqual(self.g.get_rule(Tmp), Tmp)
         self.assertEqual(self.g.rule(Tmp), Tmp)
+
+    def test_addThree(self):
+        class Tmp1(_R):
+            rule = ([NFirst], ['a', 0])
+        class Tmp2(_R):
+            rule = ([NSecond], ['a', 0, NFourth])
+        class Tmp3(_R):
+            rule = ([NThird], [0])
+        self.assertEqual(self.g.rules_count(), 0)
+        self.assertFalse(self.g.have_rule(Tmp1))
+        self.assertIsNone(self.g.get_rule(Tmp1))
+        self.assertIsNone(self.g.rule(Tmp1))
+        self.assertFalse(self.g.have_rule(Tmp2))
+        self.assertIsNone(self.g.get_rule(Tmp2))
+        self.assertIsNone(self.g.rule(Tmp2))
+        self.assertFalse(self.g.have_rule(Tmp3))
+        self.assertIsNone(self.g.get_rule(Tmp3))
+        self.assertIsNone(self.g.rule(Tmp3))
+        self.g.add_rule(Tmp1)
+        self.assertEqual(self.g.rules_count(), 1)
+        self.assertTrue(self.g.have_rule(Tmp1))
+        self.assertEqual(self.g.get_rule(Tmp1), Tmp1)
+        self.assertEqual(self.g.rule(Tmp1), Tmp1)
+        self.assertFalse(self.g.have_rule(Tmp2))
+        self.assertIsNone(self.g.get_rule(Tmp2))
+        self.assertIsNone(self.g.rule(Tmp2))
+        self.assertFalse(self.g.have_rule(Tmp3))
+        self.assertIsNone(self.g.get_rule(Tmp3))
+        self.assertIsNone(self.g.rule(Tmp3))
+        self.g.add_rule(Tmp2)
+        self.assertEqual(self.g.rules_count(), 2)
+        self.assertTrue(self.g.have_rule(Tmp1))
+        self.assertEqual(self.g.get_rule(Tmp1), Tmp1)
+        self.assertEqual(self.g.rule(Tmp1), Tmp1)
+        self.assertTrue(self.g.have_rule(Tmp2))
+        self.assertEqual(self.g.get_rule(Tmp2), Tmp2)
+        self.assertEqual(self.g.rule(Tmp2), Tmp2)
+        self.assertFalse(self.g.have_rule(Tmp3))
+        self.assertIsNone(self.g.get_rule(Tmp3))
+        self.assertIsNone(self.g.rule(Tmp3))
+        self.g.add_rule(Tmp3)
+        self.assertEqual(self.g.rules_count(), 3)
+        self.assertTrue(self.g.have_rule(Tmp1))
+        self.assertEqual(self.g.get_rule(Tmp1), Tmp1)
+        self.assertEqual(self.g.rule(Tmp1), Tmp1)
+        self.assertTrue(self.g.have_rule(Tmp2))
+        self.assertEqual(self.g.get_rule(Tmp2), Tmp2)
+        self.assertEqual(self.g.rule(Tmp2), Tmp2)
+        self.assertTrue(self.g.have_rule(Tmp3))
+        self.assertEqual(self.g.get_rule(Tmp3), Tmp3)
+        self.assertEqual(self.g.rule(Tmp3), Tmp3)
+
+    def test_addThreeInArray(self):
+        class Tmp1(_R):
+            rule = ([NFirst], ['a', 0])
+        class Tmp2(_R):
+            rule = ([NSecond], ['a', 0, NFourth])
+        class Tmp3(_R):
+            rule = ([NThird], [0])
+        self.assertEqual(self.g.rules_count(), 0)
+        self.g.add_rule([Tmp1, Tmp2, Tmp3])
+        self.assertEqual(self.g.rules_count(), 3)
+        self.assertTrue(self.g.have_rule(Tmp1))
+        self.assertEqual(self.g.get_rule(Tmp1), Tmp1)
+        self.assertEqual(self.g.rule(Tmp1), Tmp1)
+        self.assertTrue(self.g.have_rule(Tmp2))
+        self.assertEqual(self.g.get_rule(Tmp2), Tmp2)
+        self.assertEqual(self.g.rule(Tmp2), Tmp2)
+        self.assertTrue(self.g.have_rule(Tmp3))
+        self.assertEqual(self.g.get_rule(Tmp3), Tmp3)
+        self.assertEqual(self.g.rule(Tmp3), Tmp3)
+
+    def test_addThreeInTuple(self):
+        class Tmp1(_R):
+            rule = ([NFirst], ['a', 0])
+        class Tmp2(_R):
+            rule = ([NSecond], ['a', 0, NFourth])
+        class Tmp3(_R):
+            rule = ([NThird], [0])
+        self.assertEqual(self.g.rules_count(), 0)
+        self.g.add_rule((Tmp1, Tmp2, Tmp3))
+        self.assertEqual(self.g.rules_count(), 3)
+        self.assertTrue(self.g.have_rule(Tmp1))
+        self.assertEqual(self.g.get_rule(Tmp1), Tmp1)
+        self.assertEqual(self.g.rule(Tmp1), Tmp1)
+        self.assertTrue(self.g.have_rule(Tmp2))
+        self.assertEqual(self.g.get_rule(Tmp2), Tmp2)
+        self.assertEqual(self.g.rule(Tmp2), Tmp2)
+        self.assertTrue(self.g.have_rule(Tmp3))
+        self.assertEqual(self.g.get_rule(Tmp3), Tmp3)
+        self.assertEqual(self.g.rule(Tmp3), Tmp3)
 
 
 if __name__ == '__main__':
