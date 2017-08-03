72dc7c22a70b46b71fa6ed273477831632fb742e
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-03 12:35:23 +0200

    For rule invalid tests setUp method add

--- tests/rules_tests/grammarManipulation_tests/InvalidAddTest.py
+++ tests/rules_tests/grammarManipulation_tests/InvalidAddTest.py
@@ -9,10 +9,19 @@ Part of grammpy
 
 from unittest import TestCase, main
 from grammpy import Rule as _R, Grammar, Nonterminal as _N
+from ..grammar import *
 
 
 class InvalidAddTest(TestCase):
-    pass
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.g = Grammar()
+
+    def setUp(self):
+        g = Grammar()
+        g.add_term([0, 1, 2, 'a', 'b', 'c'])
+        g.add_nonterm([NFirst, NSecond, NThird, NFourth])
+        self.g = g
 
 
 if __name__ == '__main__':
