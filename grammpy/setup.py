cbfdb51ce8691a0133fd9873568eed9dad6333af
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-04 18:51:40 +0200

    Add setup file

---
+++ grammpy/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+
+setup(
+    name='grammpy',
+    version='0.1.0',
+    packages=['grammpy'],
+    url='https://github.com/PatrikValkovic/grammpy',
+    license='GNU General Public License v3.0',
+    author='Patrik Valkovic',
+    download_url='https://github.com/PatrikValkovic/grammpy/v0.1.0.tar.gz',
+    author_email='patrik.valkovic@hotmail.cz',
+    description='Package for representing formal grammars.'
+)
