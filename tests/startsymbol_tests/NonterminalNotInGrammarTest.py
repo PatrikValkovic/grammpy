95d0461cf2f06534f81a954b1f95658cbb019ec6
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-10 23:25:40 +0200

    Add tests of setting nonterminal, which is not in grammar, as start symbol

--- tests/startsymbol_tests/NonterminalNotInGrammarTest.py
+++ tests/startsymbol_tests/NonterminalNotInGrammarTest.py
@@ -12,8 +12,36 @@ from grammpy import *
 from grammpy.exceptions import NonterminalDoesNotExistsException
 
 
-class NonterminalNotInGrammarTest(TestCase):
+class A(Nonterminal):
     pass
+
+
+class B(Nonterminal):
+    pass
+
+
+class NonterminalNotInGrammarTest(TestCase):
+    def test_shouldNotSetStartSymbol(self):
+        g = Grammar(nonterminals=[A])
+        self.assertFalse(g.start_isSet())
+        with self.assertRaises(NonterminalDoesNotExistsException):
+            g.start_set(B)
+        self.assertFalse(g.start_isSet())
+        self.assertFalse(g.start_is(B))
+
+    def test_shouldNotSetStartSymbolWhenCreate(self):
+        with self.assertRaises(NonterminalDoesNotExistsException):
+            g = Grammar(nonterminals=[B],
+                        start_symbol=A)
+
+    def test_oldStartSymbolShouldStaySame(self):
+        g = Grammar(nonterminals=[A], start_symbol=A)
+        self.assertTrue(g.start_isSet())
+        with self.assertRaises(NonterminalDoesNotExistsException):
+            g.start_set(B)
+        self.assertTrue(g.start_isSet())
+        self.assertTrue(g.start_is(A))
+        self.assertEqual(g.start_get(), A)
 
 
 if __name__ == '__main__':
