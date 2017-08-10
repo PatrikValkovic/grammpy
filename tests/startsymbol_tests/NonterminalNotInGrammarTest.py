465aec2bf42d285b1707e0dbd8913856e6859e9d
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-10 23:22:27 +0200

    Add file for tests of setting invalid nonterminal as start symbol

---
+++ tests/startsymbol_tests/NonterminalNotInGrammarTest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+"""
+:Author Patrik Valkovic
+:Created 10.08.2017 23:20
+:Licence GNUv3
+Part of grammpy
+
+"""
+
+from unittest import TestCase, main
+from grammpy import *
+from grammpy.exceptions import NonterminalDoesNotExistsException
+
+
+class NonterminalNotInGrammarTest(TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    main()
