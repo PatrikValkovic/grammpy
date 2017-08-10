1d4afbea245b5c4f99266d8028022ce9a0cf32a9
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-10 22:48:49 +0200

    Promote to version 1.0.1

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 
 setup(
     name='grammpy',
-    version='1.0.0',
+    version='1.0.1',
     packages=['grammpy', 'grammpy.Grammars', 'grammpy.exceptions'],
     url='https://github.com/PatrikValkovic/grammpy',
     license='GNU General Public License v3.0',
     author='Patrik Valkovic',
-    download_url='https://github.com/PatrikValkovic/grammpy/archive/v1.0.0.tar.gz',
+    download_url='https://github.com/PatrikValkovic/grammpy/archive/v1.0.1.tar.gz',
     author_email='patrik.valkovic@hotmail.cz',
     description='Package for representing formal grammars.'
 )
