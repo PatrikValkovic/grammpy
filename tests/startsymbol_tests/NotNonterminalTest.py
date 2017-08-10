9c648d8151343f2839abdbfa6652478caa84e447
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-10 23:19:28 +0200

    Add tests of setting start symbol not nonterminal

--- tests/startsymbol_tests/NotNonterminalTest.py
+++ tests/startsymbol_tests/NotNonterminalTest.py
@@ -9,10 +9,39 @@ Part of grammpy
 
 from unittest import TestCase, main
 from grammpy import *
+from grammpy.exceptions import NotNonterminalException
+
+
+class A(Nonterminal):
+    pass
+
+
+class B(Nonterminal):
+    pass
 
 
 class NotNonterminalTest(TestCase):
-    pass
+    def test_shouldNotSetStartSymbol(self):
+        g = Grammar(nonterminals=[A, B])
+        self.assertFalse(g.start_isSet())
+        with self.assertRaises(NotNonterminalException):
+            g.start_set('asdf')
+        self.assertFalse(g.start_isSet())
+        self.assertFalse(g.start_is('asdf'))
+
+    def test_shouldNotSetStartSymbolWhenCreate(self):
+        with self.assertRaises(NotNonterminalException):
+            g = Grammar(nonterminals=[A, B],
+                        start_symbol='asdf')
+
+    def test_oldStartSymbolShouldStaySame(self):
+        g = Grammar(nonterminals=[A, B], start_symbol=A)
+        self.assertTrue(g.start_isSet())
+        with self.assertRaises(NotNonterminalException):
+            g.start_set('asdf')
+        self.assertTrue(g.start_isSet())
+        self.assertTrue(g.start_is(A))
+        self.assertEqual(g.start_get(), A)
 
 
 if __name__ == '__main__':
