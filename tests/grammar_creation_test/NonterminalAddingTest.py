1b18d81f673e9f982e9778823198cbc336285db4
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-10 22:08:21 +0200

    Add test of adding nonterminals when grammar is create

--- tests/grammar_creation_test/NonterminalAddingTest.py
+++ tests/grammar_creation_test/NonterminalAddingTest.py
@@ -9,10 +9,45 @@ Part of grammpy
 
 from unittest import TestCase, main
 from grammpy import *
+from grammpy.exceptions import NotNonterminalException
+
+
+class A(Nonterminal):
+    pass
+
+
+class B(Nonterminal):
+    pass
+
+
+class C(Nonterminal):
+    pass
+
+
+class D(Nonterminal):
+    pass
 
 
 class NonterminalAddingTest(TestCase):
-    pass
+    def test_shouldAddOneNonterminal(self):
+        g = Grammar(nonterminals=[A])
+        self.assertTrue(g.have_nonterm(A))
+        self.assertFalse(g.have_nonterm(B))
+        self.assertFalse(g.have_nonterm([A, B]))
+
+    def test_shouldAddMoreNonterminals(self):
+        g = Grammar(nonterminals=[A, B, C])
+        self.assertTrue(g.have_nonterm(A))
+        self.assertTrue(g.have_nonterm([A, B, C]))
+        self.assertFalse(g.have_nonterm(D))
+
+    def test_shouldNotAddInvalidNonterminal(self):
+        with self.assertRaises(NotNonterminalException):
+            Grammar(nonterminals=[0])
+
+    def test_shouldNotAddOneInvalidNonterminal(self):
+        with self.assertRaises(NotNonterminalException):
+            Grammar(nonterminals=[A, B, 1])
 
 
 if __name__ == '__main__':
