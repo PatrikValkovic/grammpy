18327e959a2e6c5cadbe63a7071bba8df478276a
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-10 23:01:12 +0200

    Add file fot test of start symbol setting

---
+++ tests/startsymbol_tests/SettingTest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+:Author Patrik Valkovic
+:Created 10.08.2017 23:00
+:Licence GNUv3
+Part of grammpy
+
+"""
+
+from unittest import TestCase, main
+from grammpy import *
+
+
+class SettingTest(TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    main()
