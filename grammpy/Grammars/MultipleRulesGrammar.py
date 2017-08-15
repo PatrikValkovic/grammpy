d9dafb91bfcbdeed820d3cee57f9991ebc7ec3bc
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-15 14:45:47 +0200

    Add grammar's methods to inherit

--- grammpy/Grammars/MultipleRulesGrammar.py
+++ grammpy/Grammars/MultipleRulesGrammar.py
@@ -11,4 +11,21 @@ from .StringGrammar import StringGrammar
 
 
 class MultipleRulesGrammar(StringGrammar):
-    pass
+    def __init__(self,
+                 terminals=None,
+                 nonterminals=None,
+                 rules=None,
+                 start_symbol=None):
+        super().__init__(terminals, nonterminals, rules, start_symbol)
+
+    def get_rule(self, rules=None):
+        return super().get_rule(rules)
+
+    def have_rule(self, rules):
+        return super().have_rule(rules)
+
+    def remove_rule(self, rules=None):
+        super().remove_rule(rules)
+
+    def add_rule(self, rules):
+        super().add_rule(rules)
