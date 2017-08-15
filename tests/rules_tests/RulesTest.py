ab142f01ec932faaed05441b74c4be760a963374
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-15 15:32:36 +0200

    Add file for test of behaviour when rules are pass

---
+++ tests/rules_tests/RulesTest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+:Author Patrik Valkovic
+:Created 15.08.2017 15:31
+:Licence GNUv3
+Part of grammpy
+
+"""
+
+from unittest import main, TestCase
+from grammpy import *
+
+
+class RulesTest(TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    main()
