cc003f9bf3dfe2d8f4eba8f123fb23be1d9a393c
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-16 19:19:06 +0200

    Add file for tests of grammar copy

---
+++ tests/GrammarCopyTest.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+"""
+:Author Patrik Valkovic
+:Created 16.08.2017 19:16
+:Licence GNUv3
+Part of grammpy
+
+"""
+
+from unittest import main, TestCase
+from grammpy import *
+
+
+class GrammarCopyTest(TestCase):
+    pass
+
+
+if __name__ == '__main__':
+    main()
