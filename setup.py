a3c9a2cc891da21cabe2a1f53b0f789c1136e1e7
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-16 19:42:27 +0200

    Change version to 1.1.2

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ from distutils.core import setup
 
 setup(
     name='grammpy',
-    version='1.1.1',
+    version='1.1.2',
     packages=['grammpy', 'grammpy.Grammars', 'grammpy.exceptions'],
     url='https://github.com/PatrikValkovic/grammpy',
     license='GNU General Public License v3.0',
