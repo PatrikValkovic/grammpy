2aa73c0077b9d819bda1a814d601332dbe1cab8c
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-17 23:05:14 +0200

    Implement removing of rules when term or nonterm is remove from grammar

--- grammpy/Grammars/RulesRemovingGrammar.py
+++ grammpy/Grammars/RulesRemovingGrammar.py
@@ -16,22 +16,47 @@ class RulesRemovingGrammar(PrettyApiGrammar):
                  nonterminals=None,
                  rules=None,
                  start_symbol=None):
+        self._symbs_of_rules = dict()
         super().__init__(terminals, nonterminals, rules, start_symbol)
 
-    def remove_term(self, term=None):
-        return super().remove_term(term)
-
-    def remove_nonterm(self, nonterms=None):
-        return super().remove_nonterm(nonterms)
-
     def add_term(self, term):
-        return super().add_term(term)
+        add = super().add_term(term)
+        for t in add: self._symbs_of_rules[t.s] = set()
+        return add
+
+    def remove_term(self, term=None):
+        rem = super().remove_term(term)
+        for term in rem:
+            self.remove_rule(list(self._symbs_of_rules[term.s]), _validate=False)
+            del self._symbs_of_rules[term.s]
+        return rem
 
     def add_nonterm(self, nonterms):
-        return super().add_nonterm(nonterms)
+        add = super().add_nonterm(nonterms)
+        for n in add: self._symbs_of_rules[n] = set()
+        return add
 
-    def remove_rule(self, rules=None):
-        return super().remove_rule(rules)
+    def remove_nonterm(self, nonterms=None):
+        rem = super().remove_nonterm(nonterms)
+        for nonterm in rem:
+            self.remove_rule(list(self._symbs_of_rules[nonterm]), _validate=False)
+            del self._symbs_of_rules[nonterm]
+        return rem
 
     def add_rule(self, rules):
-        return super().add_rule(rules)
+        add = super().add_rule(rules)
+        for inst in add:
+            for rule in inst.rules:
+                for symb in rule[0]:
+                    self._symbs_of_rules[symb].add(inst)
+                for symb in rule[1]:
+                    self._symbs_of_rules[symb].add(inst)
+
+    def remove_rule(self, rules=None, *, _validate=True):
+        rem = super().remove_rule(rules, _validate=_validate)
+        for inst in rem:
+            for rule in inst.rules:
+                for symb in rule[0]:
+                    self._symbs_of_rules[symb].remove(inst)
+                for symb in rule[1]:
+                    self._symbs_of_rules[symb].remove(inst)
