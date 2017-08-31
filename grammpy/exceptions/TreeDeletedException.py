d3d096e05abaaeacb4b29dbd2f3322a7b7d5d8c8
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-31 11:12:01 +0200

    Add exception that parsing tree is deleted

---
+++ grammpy/exceptions/TreeDeletedException.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+:Author Patrik Valkovic
+:Created 31.08.2017 11:10
+:Licence GNUv3
+Part of grammpy
+
+"""
+
+from .GrammpyException import GrammpyException
+
+
+class TreeDeletedException(GrammpyException):
+    pass
