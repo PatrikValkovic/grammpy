ea4e8f19271e1ac3e37b95f3e34595821dc377f5
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-31 19:16:37 +0200

    Add computing of nonterms in field slot

--- pyparsers/CYK/Field.py
+++ pyparsers/CYK/Field.py
@@ -10,27 +10,20 @@ Part of pyparsers
 from grammpy import *
 
 """
-    A B C D
-    A B C
-    A B
-    A
+
+    f[2][1] = I
+
+     x ->
+  y  A B C D
+  |  E F G
+  V  H I
+     J
 """
 
 
 class Field:
     def __init__(self, grammar: Grammar, size):
-        self._field = [[None for _ in range(k, 0, -1)] for k in range(size, 0, -1)]
-        self._termmap = dict()
-        self._rulemap = dict()
-        for r in grammar.rules():
-            if len(r.right) == 1:
-                # rule to terminal
-                if r.toSymbol not in self._termmap:
-                    self._termmap[r.toSymbol] = []
-                self._termmap[r.toSymbol].append(r)
-            else:
-                # rule with nonterms
-                key = hash(tuple(r.right))
-                if key not in self._rulemap:
-                    self._rulemap[key] = []
-                self._rulemap[key].append(r)
+        self._field = [[[] for _ in range(k, 0, -1)] for k in range(size, 0, -1)]
+
+    def nonterms(self, x, y):
+        return [r.rule for r in self._field[y][x]]
--- pyparsers/CYK/cyk.py
+++ pyparsers/CYK/cyk.py
@@ -11,7 +11,26 @@ from typing import Iterable
 from grammpy import *
 from .Field import Field
 
+def _create_mapping(grammar: Grammar) -> tuple:
+    termmap = dict()
+    rulemap = dict()
+    for r in grammar.rules():
+        if len(r.right) == 1:
+            # rule to terminal
+            if r.toSymbol not in termmap:
+                termmap[r.toSymbol] = []
+            termmap[r.toSymbol].append(r)
+        else:
+            # rule with nonterms
+            key = hash(tuple(r.right))
+            if key not in rulemap:
+                rulemap[key] = []
+            rulemap[key].append(r)
+    return (termmap, rulemap)
+
+
 def cyk(grammar: Grammar, input: Iterable) -> Nonterminal:
     i = list(input)
     f = Field(grammar, len(i))
+    (termmap, rulemap) = _create_mapping(grammar)
     raise NotImplementedError()
