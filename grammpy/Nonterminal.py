626ae46bfdf8643cda369ad520e8f12d933b9b55
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-08-31 11:13:16 +0200

    Add from_rule and to_rule properties into Nonterminal

--- grammpy/Nonterminal.py
+++ grammpy/Nonterminal.py
@@ -7,6 +7,27 @@ Part of grammpy
 
 """
 
+import weakref
+from .exceptions import TreeDeletedException
+
 
 class Nonterminal:
-    pass
+    def __init__(self):
+        self._from_rule = None
+        self._to_rule = None
+
+    @property
+    def from_rule(self):
+        if self._from_rule() is None:
+            raise TreeDeletedException()
+        return self._from_rule()
+
+    @property
+    def to_rule(self):
+        return self._to_rule
+
+    def _set_from_rule(self, r):
+        self._from_rule = weakref.ref(r)
+
+    def _set_to_rule(self, r):
+        self._to_rule = r
