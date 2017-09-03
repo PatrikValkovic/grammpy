37b0f5b2d16e4881bc6d341ce055949bb0476be6
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2017-09-03 10:43:07 +0200

    Add setup file

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+v='0.0.1'
+
+setup(
+    name='pyparsers',
+    version=v,
+    packages=['pyparsers', 'pyparsers.CYK'],
+    url='https://github.com/PatrikValkovic/pyparsers',
+    license='GNU General Public License v3.0',
+    download_url='https://github.com/PatrikValkovic/pyparsers/archive/v' + v + '.tar.gz',
+    author='Patrik Valkovic',
+    author_email='patrik.valkovic@hotmail.cz',
+    description='Parsers for grammpy library',
+    install_requires=[
+        'grammpy'
+    ]
+)
