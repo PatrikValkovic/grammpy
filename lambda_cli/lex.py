557530cfc5b9bf62b25aec23bff00593f44bf437
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2018-03-30 14:57:50 +0200

    Created lexer

--- lambda_cli/lex.py
+++ lambda_cli/lex.py
@@ -10,6 +10,74 @@ Part of lambda-cli
 from ply import lex
 from .terminals import *
 
+states = (
+    ('parameters', 'exclusive'),
+)
+
+tokens = (
+    'LAMBDA',
+    'DOT',
+    'NUMBER',
+    'VARIABLE',
+    'PARAMETER',
+    'LEFTBRACKET',
+    'RIGHTBRACKET',
+)
+
+t_INITIAL_parameters_ignore = ' \t'
+
+
+def t_INITIAL_LAMBDA(t):
+    r'lambda'
+    t.value = LambdaKeyword
+    t.lexer.begin('parameters')
+    return t
+
+
+def t_parameters_DOT(t):
+    r'\.'
+    t.value = Dot
+    t.lexer.begin('INITIAL')
+    return t
+
+
+def t_INITIAL_NUMBER(t):
+    r'\d+'
+    t.value = Number(int(t.value))
+    return t
+
+
+def t_INITIAL_VARIABLE(t):
+    r'[a-zA-Z\']+'
+    t.value = Variable(t.value)
+    return t
+
+
+def t_parameters_PARAMETER(t):
+    r'[a-zA-Z\']+'
+    t.value = Parameter(t.value)
+    return t
+
+
+def t_INITIAL_LEFTBRACKET(t):
+    r'\('
+    t.value = LeftBracket
+    return t
+
+
+def t_INITIAL_RIGHTBRACKET(t):
+    r'\)'
+    t.value = RightBracket
+    return t
+
+
+lexer = lex.lex()
+
 
 def lambda_cli_lex(input):
-    return input
+    lexer.input(input)
+    while True:
+        tok = lexer.token()
+        if not tok:
+            return  # No more input
+        yield tok.value
