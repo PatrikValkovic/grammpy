d2850739bc1963e271e68a49c81c952f5fefd319
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2018-03-31 12:15:44 +0200

    Add dependency on typing

--- setup.py
+++ setup.py
@@ -20,5 +20,7 @@ setup(
     download_url='https://github.com/PatrikValkovic/grammpy/archive/v1.0.1.tar.gz',
     author_email='patrik.valkovic@hotmail.cz',
     description='Package for representing formal grammars.',
-    install_requires=[],
+    install_requires=[
+        'typing'
+    ],
 )
